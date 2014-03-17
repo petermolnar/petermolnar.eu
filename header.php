81fc7525ea49684c15b8d02aef3ef93c1f730c64
Peter Molnar <hello@petermolnar.eu>
2014-03-17 18:47:37 +0000

    page: no footer & small updates

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title><?php wp_title( ); ?></title>
 	<link rel="author" href="https://plus.google.com/117393351799968573179/posts" />
 	<link rel="publisher" href="https://plus.google.com/117393351799968573179/posts" />
--- template-page.php
+++ template-page.php
@@ -5,7 +5,4 @@
 
 <arcticle id="page-<?php the_ID(); ?>" class="single-arcticle">
 	<?php the_content(); ?>
-	<footer>
-		<?php $petermolnareu_theme->share ( get_permalink() , wp_title( '', false ), $post->ID ); ?>
-	</footer>
 </arcticle>
